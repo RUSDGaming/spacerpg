b1aae6fcb20a46839977e433656e6b183b5c0cda
Shane Dublin <rushanedublin@gmail.com>
2015-10-15 02:00:56 -0700

    setting up player controllrer

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private float horizontal;
+    private float vertical;
+    public float speed;
+
+    private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontal = Input.GetAxisRaw("Horizantal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+	}
+
+    void FixedUpdate()
+    {
+        body.AddForce(new Vector2(horizontal, vertical));
+    }
+
+}
